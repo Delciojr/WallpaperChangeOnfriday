f917a0f90512d8afb148f498c1160b19d8ac9e1d
Delcio Derigo Junior <84386201+Delciojr@users.noreply.github.com>
2023-02-10 08:39:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+const isSexta = async()=>{
+    let hoje = dayjs().format()
+    let hojeEsexta = dayjs(hoje).get("day")
+
+    return  hojeEsexta == 5
+}
+
+
+const start = async()=>{
+    let eSexta =await isSexta()
+    if(eSexta){
+        document.getElementById('wallpaper-1').hidden=false;
+        document.getElementById('wallpaper-2').hidden=true;
+    }else{
+        document.getElementById('wallpaper-1').hidden=true;
+        document.getElementById('wallpaper-2').hidden=false;
+    }
+}
+
+
+start()
